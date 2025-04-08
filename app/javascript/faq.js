4db9b251a28deac769cd382809b88e2c651c0e06
Rodrigo Alvarez de Toledo <alvarezdetoledo14@gmail.com>
2025-04-08 19:15:18 +0000

    home avanzada

---
+++ app/javascript/faq.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+      const item = button.parentElement;
+      item.classList.toggle('active');
+    });
+  });
+});
