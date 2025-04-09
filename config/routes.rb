4ee6fe28cf9c397b92ab924a6efd4372bf9fcc85
Rodrigo Alvarez de Toledo <alvarezdetoledo14@gmail.com>
2025-04-09 21:09:33 +0000

    navbar finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resource :profile, only: [:edit, :update], controller: 'profiles'
   root to: "pages#home"
+  get "/plans", to: "pages#plans", as: :plans
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
