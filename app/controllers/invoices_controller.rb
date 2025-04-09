fd8f289c93540514bb9e406378f69dea98bc942d
jorge gonzalez <gonzalezjor13@gmail.com>
2025-04-09 17:06:44 -0500

    invoice bastante avanzada

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,10 @@
 class InvoicesController < ApplicationController
   def index
+    @invoices = Invoice.all
   end
 
   def show
+    @invoice = Invoice.find(params[:id])
   end
 
   def edit
@@ -12,11 +14,39 @@ class InvoicesController < ApplicationController
   end
 
   def destroy
+    @invoice = Invoice.find(params[:id])
+    @invoice.destroy
+    redirect_to invoices_path, status: :see_other
   end
 
   def new
+    @invoice = Invoice.new
+    @detail_invoice = @invoice.detail_invoices.build
+
+
+    @companies = Company.all
+    @clients = Client.all
   end
 
+
   def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      @invoice.calculated_totals
+      redirect_to @invoice, notice: 'Factura creada con éxito.'
+    else
+      render :new
+    end
   end
+
+
+  private
+
+    def invoice_params
+      params.require(:invoice).permit(
+        :company_id, :client_id, :payment_type, :payment_method, :sub_total, :taxes, :total,
+        detail_invoices_attributes: [:product_number, :amount, :unit_value, :description]
+      )
+    end
 end
