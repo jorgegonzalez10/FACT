08c360e4db1c46f39fa3f4564ba7059853634383
Olivia Calderon <o.c.o.informatica@gmail.com>
2025-04-10 15:27:53 -0600

    agregue en controlador de dashboard y en index de dash y en sccs de dash

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,11 @@ class DashboardController < ApplicationController
     @clientes_por_mes = Client.group_by_month(:created_at, format: "%B", locale: :es).count
     @clientes_por_mes = Client.group_by_month(:created_at, format: "%B", locale: :es).count
     @empresas_por_mes = Company.group_by_month(:created_at, format: "%B", locale: :es).count
+    @invoices_by_month = Invoice.group_by_month(:created_at, format: "%B", locale: :es).count
+    @total_invoice_amount_by_month = Invoice.group_by_month(:created_at, format: "%B", locale: :es).sum(:total)
+    @invoices_by_company = Invoice.joins(:company)
+                               .group('companies.name')
+                               .count
 
     @resumen_por_tipo = {
       "Clientes" => Client.count,
